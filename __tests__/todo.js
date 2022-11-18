61000380e903e2ea2ed93c0a9c19d075f264c6ae
Sachin-kr21 <sachin210302@gmail.com>
2022-11-19 00:31:42 +0530

    hello i am sachin

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,12 +1,40 @@
 const todoList = require('../todo');
-const {all , markAsComplete , add } = todoList();
+const {all , markAsComplete , add,overdue, dueToday, dueLater } = todoList();
 describe("Todolist Test Suite", () => {
     beforeAll(() => {
         add(
             {
-                title: "Test todo",
+                title: "Test todo1",
                 completed: false,
                 dueDate: new Date().toLocaleDateString("en-CA")
+            }
+        );
+        add(
+            {
+                title: "Test todo2",
+                completed: false,
+                dueDate: new Date(new Date().setDate(new Date().getDate() - 2)).toLocaleDateString("en-CA")
+            }
+        );
+        add(
+            {
+                title: "Test todo3",
+                completed: false,
+                dueDate: new Date(new Date().setDate(new Date().getDate() + 2)).toLocaleDateString("en-CA")
+            }
+        );
+        add(
+            {
+                title: "Test todo2",
+                completed: false,
+                dueDate: new Date(new Date().setDate(new Date().getDate() - 5)).toLocaleDateString("en-CA")
+            }
+        );
+        add(
+            {
+                title: "Test todo2",
+                completed: false,
+                dueDate: new Date(new Date().setDate(new Date().getDate() + 5)).toLocaleDateString("en-CA")
             }
         );
     })
@@ -26,4 +54,23 @@ describe("Todolist Test Suite", () => {
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
     })
+    test("Should check retrieval of overdue items",()=>{
+        todaysDate=new Date().toLocaleDateString("en-CA")
+        overdue().forEach(e=>{
+            expect(e.dueDate<todaysDate).toBe(true)
+        })
+    })
+    test("Should checks retrieval of due today items",()=>{
+        todaysDate=new Date().toLocaleDateString("en-CA")
+        dueToday().forEach(e=>{
+            expect(e.dueDate===todaysDate).toBe(true)
+        })
+    })
+    test("Should checks retrieval of due later items",()=>{
+        todaysDate=new Date().toLocaleDateString("en-CA")
+        dueLater().forEach(e=>{
+            expect(e.dueDate>todaysDate).toBe(true)
+        })
+    })
+
 })
